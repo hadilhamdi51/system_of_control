0d87294f3215cd00e681793569b6e6973fc991ca
Hadil Hamdi <103290208+hadilhamdi51@users.noreply.github.com>
2022-05-03 22:03:26 +0200

    testing

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    return" hello hadil hamdi";
+    return" hello hadil hamdi how are you";
 });
