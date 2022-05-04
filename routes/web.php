4d2fbf91d959ebc87949f056bf8b7709a35e266d
hadil hamdi <hadilfb51@gmail.com>
2022-05-04 13:43:05 +0200

    test hadil

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    return" hello hadil hamdi how are you didi";
+    return" hello hadil hamdi ";
 });
 Route::get('/testmalek', function () {
     return view('welcome Malek Kraimia');
