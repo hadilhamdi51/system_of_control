25fc4883b4975c43aa2d6a03352f6cf2e883f198
hadil hamdi <hadilfb51@gmail.com>
2022-05-06 00:56:29 +0200

    Create api/UserController & api/ReclamationController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\ReclamationController;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-    
-});
+Route::get('/',[AppController::class, 'welcome'] );
+Route::resource('/users',UserController::class);
+Route::resource('/reclamations',ReclamationController::class);
