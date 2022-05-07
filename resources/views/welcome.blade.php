21683147b8c5d764d9df9d0d3f41ef13802b1032
hadil hamdi <hadilfb51@gmail.com>
2022-05-08 07:03:22 +1300

    template part 7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -291,7 +291,7 @@
 				</div>
 			</nav>
 
-			<main class="content">
+			<main class="content mt-9">
 				<div class="container-fluid p-0">
 
 					<h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
