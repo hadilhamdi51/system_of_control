01cdb40882d8c62b8cd6358af1098da3c5671a8d
hadil hamdi <hadilfb51@gmail.com>
2022-05-13 06:40:07 +1300

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\ReclamationController;
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[AppController::class, 'welcome'] );
 Route::resource('/users',UserController::class);
 Route::resource('/reclamations',ReclamationController::class);
+Route::get('/auth/login',[MainController::class,'login']);
