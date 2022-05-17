3997f86a26586492e00470cd034bc9358b17dda2
hadil hamdi <hadilfb51@gmail.com>
2022-05-18 04:53:28 +1300

    dash

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
-
+use App\Http\Controllers\AuthenticatedSessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
