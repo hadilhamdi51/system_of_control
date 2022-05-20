7b3a95b3e68fdc2e927adeaa8a5e050354d1bcfa
hadil hamdi <hadilfb51@gmail.com>
2022-05-21 09:15:59 +1300

    users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,7 +24,44 @@ class UserSeeder extends Seeder
                 'name'=>'user',
                 'email'=>'user@gmail.com',
                 'admin'=>0,
-                'password'=>\Hash::make('1234567')]
+                'password'=>\Hash::make('1234567')],
+
+                
+            [
+                'name'=>'Hadil Hamdi ',
+                'email'=>'hamdihadil51@gmail.com',
+                'admin'=>1,
+                'password'=>\Hash::make('hadil123'),
+                'image'=>'img\avatars\dila.jpg',
+
+            ],
+            [
+                'name'=>'Oumaima Ben Amor ',
+                'email'=>'oumaima@gmail.com',
+                'admin'=>0,
+                'password'=>\Hash::make('oumaima123'),
+                'image'=>'img\avatars\ouma.jpeg',
+
+            ],
+            [
+                'name'=>'Melek Kraimia  ',
+                'email'=>'melek@gmail.com',
+                'admin'=>0,
+                'password'=>\Hash::make('melek123'),
+                'image'=>'img\avatars\ouma.jpg',
+
+            ],
+            [
+                'name'=>'Ines Khiari ',
+                'email'=>'ines@gmail.com',
+                'admin'=>0,
+                'password'=>\Hash::make('iness123'),
+                'image'=>'img\avatars\ines.jpg',
+
+            ]
+
+
+
 
         ];
         foreach($users as $key =>$value ){
